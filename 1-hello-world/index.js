296f60d5ce94fb39fa157412eae66add24c232bc
TuanDung <38681344+TuanDungg@users.noreply.github.com>
2020-04-06 15:22:52 +0700

    'add'

--- 1-hello-world/index.js
+++ 1-hello-world/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(request, response){
-    response.send('Hello world!');
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res){
+    res.render('index', {
+        name: 'Dũng'
+    });
+});
+
+app.get('/users', function(req, res){
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Dũng'},
+            { id: 2, name: 'Tuấn'}
+        ]
+    });
 });
 
 app.listen(port, function(){
