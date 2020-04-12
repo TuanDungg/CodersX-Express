a29bb385d2789df90302d745ed7874922312c64e
TuanDung <38681344+TuanDungg@users.noreply.github.com>
2020-04-12 16:48:54 +0700

    Create user validation

--- 1-hello-world/index.js
+++ 1-hello-world/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
-var app = express();
-var port = 3000;
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-var db = low(adapter);
-var shortid = require('shortid');
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write()
+var usersRoute = require('./routes/users.route');
+
+var app = express();
+
+var port = 3000;
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -17,6 +12,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.render('index', {
@@ -24,40 +20,7 @@ app.get('/', function(req, res){
     });
 });
 
-app.get('/users', function(req, res){
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
-
-app.get('/users/search', function(req, res){
-    var q = req.query.q;
-    var matchedUsers = db.get('users').value().filter(function(user){
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    res.render('users/index', {
-        users: matchedUsers
-    });
-});
-
-app.get('/users/create', function(req, res){
-    res.render('users/create');
-});
-
-app.get('/users/:id', function(req, res){
-    var id = req.params.id;
-    var user = db.get('users').find({ id: id }).value();
-
-    res.render('users/view', {
-        user: user
-    });
-});
-
-app.post('/users/create', function(req, res){
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
-});
+app.use('/users', usersRoute);
 
 app.listen(port, function(){
     console.log('Server listening on port' + port);
